53a8f69c1b1be588b123d5a7165b1cc15aeb6e08
Ronan Doherty <donanroherty@gmail.com>
2018-06-02 15:29:32 +0100

    Rolled back changes to typography js

---
+++ src/utils/typography.js
@@ -0,0 +1,8 @@
+import Typography from 'typography'
+
+import funstonTheme from 'typography-theme-funston'
+
+// const typography = new Typography(funstonTheme)
+const typography = new Typography({ baseFontSize: '18px' })
+
+export default typography
