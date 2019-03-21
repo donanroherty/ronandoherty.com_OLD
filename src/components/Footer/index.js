bc3cc3fa6050c38ab04b60d3a31707604376b127
Ronan Doherty <donanroherty@gmail.com>
2019-03-21 09:39:16 +0000

    Footer style tweaks

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,30 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
-import {Line} from '../Utils/common-elements'
 
-const Content = styled.div `
+const Content = styled.div`
   width: 100%;
   text-align: right;
-  /* margin-left: auto; */
-  /* max-width: 370px; */
-  color: ${props => props.theme.color2};
-  
+  color: ${props => props.theme.colorLightText};
   height: 40px;
-
-  /* display: grid;
-  grid-template-columns: auto;
-  grid-template-rows: auto auto;
-  grid-gap: 16px; */
 `
 
 const Header = props => {
-  const iconSize = 24
-  return (
-    <Content>
-      <Line/>
-      ©2018 {props.siteMetadata.title}
-    </Content>
-  )
+  return <Content>©2018 {props.siteMetadata.title}</Content>
 }
 
 export default Header
